5817a6e54805d617d7c0d5d4ecb439e17f35d55d
霍荣慧 <huorh@missfresh.cn>
2020-05-31 18:37:07 +0800

    cli-2.0.0；更换图标

--- GoToNetUI-X/UI/AboutWindowController.swift
+++ GoToNetUI-X/UI/AboutWindowController.swift
@@ -16,8 +16,6 @@ class AboutWindowController: NSWindowController {
     
     @IBOutlet weak var git: NSTextField!
     
-    @IBOutlet weak var iconAuthor: NSTextField!
-    
     override func windowDidLoad() {
         super.windowDidLoad()
 
@@ -28,17 +26,6 @@ class AboutWindowController: NSWindowController {
         self.git.backgroundColor = .clear
         self.git.isBordered = false
         self.git.isSelectable = true
-        
-        self.iconAuthor.textColor = .linkColor
-        self.iconAuthor.backgroundColor = .clear
-        self.iconAuthor.isBordered = false
-        self.iconAuthor.isSelectable = true
-        self.iconAuthor.allowsEditingTextAttributes = true
-        
-        let attribute = NSMutableAttributedString()
-        attribute.append(NSAttributedString(string: "点击这里", attributes: [NSAttributedString.Key.link: "https://www.iconfont.cn/collections/detail?spm=a313x.7781069.1998910419.d9df05512&cid=2692"]))
-        self.iconAuthor.attributedStringValue = attribute
-        
     }
     
 }
